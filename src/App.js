12f58af3aaeb60a9437a007f8a87bfa318607638
max.ludwig <max.ludwig@digitecgalaxus.ch>
2022-11-24 13:32:47 +0100

    Implemented Start and partially the hit button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import { StartButton } from "./start/StartButton";
 
 function App() {
+  const [playerState, setPlayerState] = useState();
+  const [dealerState, setDealerState] = useState();
+  const card = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+
+  const symbols = ["Hearts", "Clubs", "Diamonds", "Spades"];
+
+  const getRandomCard = () => {
+    const randomNumberCard = Math.floor(Math.random() * card.length);
+    const randomNumberSymbol = Math.floor(Math.random() * symbols.length);
+    const randomCard = card.slice(randomNumberCard, randomNumberCard + 1);
+    const randomSymbol = symbols.slice(
+      randomNumberSymbol,
+      randomNumberSymbol + 1
+    );
+
+    return [randomCard, randomSymbol];
+  };
+
   return (
     <div className="App">
       <div className="size ">
@@ -13,8 +46,31 @@ function App() {
           <img src="./titel.jpg" alt="cards" className="titel-img"></img>
         </div>
       </div>
+      <div className="App-header">
+        {!playerState && (
+          <StartButton
+            onClick={() => {
+              const dealerRandomCard = getRandomCard();
+              setDealerState(dealerRandomCard);
+              const randomCard = getRandomCard();
+              setPlayerState(randomCard);
+            }}
+          ></StartButton>
+        )}
+        {playerState && (
+          <button
+            onClick={() => {
+              const randomCard = getRandomCard();
+              setPlayerState([...playerState, randomCard]);
+            }}
+          >
+            Hit
+          </button>
+        )}
 
-      <div className="App-header"></div>
+        {playerState && <p>Player{playerState.join(",")}</p>}
+        {dealerState && <p>Dealer{dealerState.join(",")}</p>}
+      </div>
     </div>
   );
 }
---
+++ src/start/StartButton.jsx
@@ -0,0 +1,3 @@
+export const StartButton = ({onClick}) =>{
+    return <button onClick={onClick}>Start</button>
+}
