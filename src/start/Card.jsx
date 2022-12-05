ead9d8683dee16ac565cc248c6a42faf154220f7
kkhaali <111353551+kkhaali@users.noreply.github.com>
2022-12-05 10:23:15 +0100

    no message

--- src/start/Card.jsx
+++ src/start/Card.jsx
@@ -1,11 +1,11 @@
-import '../Cards.css';
+import './Cards.css';
 
 export const Card = ({value, symbol}) => {
     let entity;
-    entity = "&" + symbol.toLowerCase() + ";"
-    return <div>
+    symbol === "Diamonds" ? (entity = "&diams;") : (entity = "&" + symbol.toLowerCase() + ";")
+    return <div className={`card ${symbol.toLowerCase()}`}>
         <span className="card-value-suit top">{value} <span dangerouslySetInnerHTML={{__html: entity}}/></span>
         <span className="card-suit"><span dangerouslySetInnerHTML={{__html: entity}}/></span>
-        <span className="card-value-suit bot">{value} <span dangerouslySetInnerHTML={{__html: entity}}/></span>;
+        <span className="card-value-suit bot">{value} <span dangerouslySetInnerHTML={{__html: entity}}/></span>
         </div> 
    };
